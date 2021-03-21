c3fc78fc7cf2bea88cf612d721903abeceb75d40
Mateo Florido <mateoflosan@gmail.com>
2021-03-21 18:15:12 -0500

    Created Cart and CartItem Entities

---
+++ src/main/java/com/onebuilder/backend/entity/Cart.java
@@ -0,0 +1,41 @@
+package com.onebuilder.backend.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+@Entity
+public class Cart {
+    @Id
+    private Long id;
+    @OneToOne
+    private User user;
+    @OneToMany
+    private List<CartItem> cartItems;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+}
