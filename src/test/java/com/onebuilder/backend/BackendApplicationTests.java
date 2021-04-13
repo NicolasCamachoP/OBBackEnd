aa90de22ca31888d1c66ea1622696b4fc95fe4ee
Nicolás <nicolas-camacho@javeriana.edu.co>
2021-04-13 17:13:04 -0500

    @SpringBootTest annotation fixed

--- src/test/java/com/onebuilder/backend/BackendApplicationTests.java
+++ src/test/java/com/onebuilder/backend/BackendApplicationTests.java
@@ -1,9 +1,10 @@
 package com.onebuilder.backend;
 
+import com.onebuilder.backend.main.BackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
