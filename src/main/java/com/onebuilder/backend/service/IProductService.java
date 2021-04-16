d9c473bc94e7736b5e53beca0acea75e3c9210dd
Mateo Florido <mateoflosan@gmail.com>
2021-04-16 16:45:11 -0500

    Created necessary methods for Product repository

--- src/main/java/com/onebuilder/backend/service/IProductService.java
+++ src/main/java/com/onebuilder/backend/service/IProductService.java
@@ -1,4 +1,25 @@
 package com.onebuilder.backend.service;
 
+import com.onebuilder.backend.entity.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface IProductService {
+    Product createProduct(Product p);
+
+    Product getProductByEAN(String EAN);
+
+    List<Product> getProducts();
+
+    List<Product> getProductWithStock();
+
+    void deleteProduct(Long id);
+
+    Product updateProduct(Product p, Long id);
+
+    void updateProductStock(String EAN);
+
+
 }
